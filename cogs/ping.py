12f27b412c1b482d0fb1cde35e3d71189fa635fb
seth <117851610+Sethispr@users.noreply.github.com>
2024-12-10 21:13:13 -0800

    ruff format

--- cogs/ping.py
+++ cogs/ping.py
@@ -6,6 +6,7 @@ It also defines a listener to print a message when the cog is ready.
 import discord
 from discord.ext import commands
 
+
 class PingCog(commands.Cog):
     """
     A cog for handling ping-related commands and events for the bot.
@@ -14,7 +15,7 @@ class PingCog(commands.Cog):
     def __init__(self, bot):
         """
         Initialize the PingCog with the bot instance.
-        
+
         Args:
             bot (discord.Bot): The bot instance.
         """
@@ -29,22 +30,45 @@ class PingCog(commands.Cog):
         print("PingCog is ready.")
 
     @discord.app_commands.command(name="ping", description="Check the bot's latency")
-    async def ping(self, interaction: discord.Interaction):
+    async def ping_slash(self, interaction: discord.Interaction):
         """
-        Respond with the bot's latency in milliseconds.
-        
+        Respond with the bot's latency in milliseconds for slash commands.
+
         Args:
             interaction (discord.Interaction): The interaction object representing the command.
         """
-        latency = round(self.bot.latency * 1000)  # Convert latency to milliseconds
+        latency = round(self.bot.latency * 1000)
         embed = discord.Embed(
             title="Pong!",
             description=f"Latency is {latency}ms. <:ping:1315726966036496536>",
-            color=discord.Color.from_rgb(250, 254, 99)
+            color=discord.Color.from_rgb(250, 254, 99),
         )
-        embed.set_footer(icon_url=self.bot.user.avatar.url if self.bot.user.avatar else None)
+        embed.set_footer(
+            icon_url=self.bot.user.avatar.url if self.bot.user.avatar else None
+        )
         embed.timestamp = discord.utils.utcnow()
         await interaction.response.send_message(embed=embed, ephemeral=True)
+
+    @commands.command(name="ping")
+    async def ping_text(self, ctx):
+        """
+        Respond with the bot's latency in ms for prefix text commands.
+
+        Args:
+            ctx (commands.Context): The context in which the command was invoked.
+        """
+        latency = round(self.bot.latency * 1000)
+        embed = discord.Embed(
+            title="Pong!",
+            description=f"Latency is {latency}ms. <:ping:1315726966036496536>",
+            color=discord.Color.from_rgb(250, 254, 99),
+        )
+        embed.set_footer(
+            icon_url=self.bot.user.avatar.url if self.bot.user.avatar else None
+        )
+        embed.timestamp = discord.utils.utcnow()
+        await ctx.send(embed=embed)
+
 
 async def setup(bot):
     """
